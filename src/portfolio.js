4cd78c9c70355ada11b5d04a379b4723673b309f
Bruno de Oliveira <brn.olives@gmail.com>
2024-03-12 16:22:15 -0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,14 +6,14 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Bruno de Oliveira',
+  role: 'Cientista de Dados',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/o-brn/',
+    github: 'https://github.com/BrnCode/',
   },
 }
 
@@ -49,23 +49,17 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'SQL',
+  'Python',
+  'Excel',
+  'PowerBI',
+  'Flask',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'brn.olives@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
