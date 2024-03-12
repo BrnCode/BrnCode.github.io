a62459f1f6b57a36e4d1f01a4c42050df7281ea5
Bruno de Oliveira <brn.olives@gmail.com>
2024-03-12 16:20:26 -0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://brncode.github.io/',
   title: 'JS.',
 }
 
